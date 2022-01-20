d9ecc8b571345067935937fe17b340ac61d7fd2e
dva <60975283+dvaRepo@users.noreply.github.com>
2022-01-19 21:35:34 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,10 @@
-# install basic packages and configure basic setup
+#! /bin/bash
+
+# aur helper
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
+cd ..
+
+sudo pacman -S --needed - < pkglist.txt
