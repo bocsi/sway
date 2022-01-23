ab2dc33906fe3f6291500a50e79c44f453bfbc41
dva <60975283+dvaRepo@users.noreply.github.com>
2022-01-22 21:43:55 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -34,3 +34,12 @@ yay -S auto-cpufreq
 sudo systemctl enable auto-cpufreq
 sudo systemctl start auto-cpufreq
 sudo systemctl enable bluetooth
+
+# hp laptop
+# git clone https://aur.archlinux.org/wd719x-firmware.git
+# cd wd719x-firmware
+# makepkg -sri
+# git clone https://aur.archlinux.org/aic94xx-firmware.git
+# cd aic94xx-firmware
+# makepkg -sri
+# yay -S upd72020x-fw
