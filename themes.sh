60b4f3e44fc444d10cae093afa277f4df343a7c4
dva <60975283+dvaRepo@users.noreply.github.com>
2022-01-22 22:03:47 -0800

    Update themes.sh

--- themes.sh
+++ themes.sh
@@ -3,14 +3,14 @@ git clone https://github.com/vinceliuice/Orchis-theme.git
 cd Orchis-theme/
 ./install.sh --tweaks compact
 cd ..
-rm -r Orchis-theme
+rm -rf Orchis-theme
 
 # icon theme
 git clone https://github.com/yeyushengfan258/Reversal-icon-theme.git
 cd Reversal-icon-theme/
 ./install.sh -blue
 cd ..
-rm -r Reversal-icon-theme
+rm -rf Reversal-icon-theme
 
 git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git
 cd WhiteSur-gtk-theme/
