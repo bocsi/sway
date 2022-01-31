9d64ee6fd2a3170bdbfd284bbcdb25e023e206ba
dva <60975283+dvaRepo@users.noreply.github.com>
2022-01-31 15:58:27 -0800

    Update themes.sh

--- themes.sh
+++ themes.sh
@@ -1,19 +1,13 @@
 # gtk theme
-git clone https://github.com/vinceliuice/Orchis-theme.git
-cd Orchis-theme/
-./install.sh --tweaks compact
+git clone https://github.com/vinceliuice/Colloid-gtk-theme.git
+cd Colloid-gtk-theme
+./install.sh --tweaks rimless normal
 cd ..
-rm -rf Orchis-theme
-
 # icon theme
-git clone https://github.com/yeyushengfan258/Reversal-icon-theme.git
-cd Reversal-icon-theme/
-./install.sh -blue -black
+cd icon-theme
+./install.sh
 cd ..
-rm -rf Reversal-icon-theme
+cd..
 
-git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git
-cd WhiteSur-gtk-theme/
-./install.sh -c dark -t blue -i debian
-cd ..
-rm -rf WhiteSur-gtk-theme/
+#clean up
+rm -rf Colloid-gtk-theme
