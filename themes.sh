a3ff99188b891370b18c41d49d2d8c04a9ca0636
dva <60975283+dvaRepo@users.noreply.github.com>
2022-04-01 15:49:27 -0700

    Update themes.sh

--- themes.sh
+++ themes.sh
@@ -2,11 +2,13 @@
 git clone https://github.com/vinceliuice/Colloid-gtk-theme.git
 cd Colloid-gtk-theme
 ./install.sh --tweaks rimless normal
-# icon theme
-cd icon-theme
-./install.sh
 cd ..
+# icon theme
+git clone https://github.com/vinceliuice/Colloid-icon-theme.git
+cd Colloid-icon-theme
+./install.sh
 cd..
 
 #clean up
 rm -rf Colloid-gtk-theme
+rm -rf Colloid-icon-theme
