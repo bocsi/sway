e9a2be7cb49fd6c56a489ff55b6b2c39ebfd40d0
dva <60975283+dvaRepo@users.noreply.github.com>
2022-04-01 16:20:49 -0700

    Update themes.sh

--- themes.sh
+++ themes.sh
@@ -1,3 +1,12 @@
+#dependencies
+DISTRO=`cat /etc/os-release | head -1 | sed 's/NAME="//' | cut -d ' ' -f 1`
+if [ "$DISTRO" == "Arch" ]; then
+    sudo pacman -S gtk-engine-murrine
+elif [ "$DISTRO" == "Fedora" ]; then
+    sudo dnf install gtk-murrine-engine
+elif [ "$DISTRO" == "Debian" ]; then
+    sudo apt install gtk2-engines-murrine
+fi
 # gtk theme
 git clone https://github.com/vinceliuice/Colloid-gtk-theme.git
 cd Colloid-gtk-theme
